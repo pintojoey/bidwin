e0f5f0f54a73789c299b2aecd7b70baeef5fffca
Joey Pinto <pintojoey@gmail.com>
2018-08-26 12:25:33 -0700

    Added Node.js Base API Server

--- src/main/java/bidwin/base/Keygen.java
+++ src/main/java/bidwin/base/Keygen.java
@@ -14,29 +14,29 @@ import java.security.NoSuchAlgorithmException;
 
 public class Keygen {
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-//        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
-//        keyGen.initialize(512);
-//        KeyPair pair = keyGen.genKeyPair();
-//        byte[] publicKey = pair.getPublic().getEncoded();
-//
-//        StringBuffer retString = new StringBuffer();
-//        for (int i = 0; i < publicKey.length; ++i) {
-//            retString.append(Integer.toHexString(0x0100 + (publicKey[i] & 0x00FF)).substring(1));
-//        }
-//        System.out.println("public key: " +retString);
-//
-//        byte[] privateKey = pair.getPrivate().getEncoded();
-//
-//
-//         retString = new StringBuffer();
-//        for (int i = 0; i < privateKey.length; ++i) {
-//            retString.append(Integer.toHexString(0x0100 + (privateKey[i] & 0x00FF)).substring(1));
-//        }
-//        System.out.println("private key: " +retString);
-//
-//        Cipher cipher = Cipher.getInstance(pair.getPrivate().getAlgorithm());
-//        cipher.init(Cipher.ENCRYPT_MODE, pair.getPrivate());
-//        System.out.println("sig: "+cipher.doFinal("{}".getBytes(Charset.defaultCharset())).toString());
-//        System.out.println(Hex.encodeHexString(cipher.doFinal(("Hello world").getBytes())));
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(512);
+        KeyPair pair = keyGen.genKeyPair();
+        byte[] publicKey = pair.getPublic().getEncoded();
+
+        StringBuffer retString = new StringBuffer();
+        for (int i = 0; i < publicKey.length; ++i) {
+            retString.append(Integer.toHexString(0x0100 + (publicKey[i] & 0x00FF)).substring(1));
+        }
+        System.out.println("public key: " +retString);
+
+        byte[] privateKey = pair.getPrivate().getEncoded();
+
+
+         retString = new StringBuffer();
+        for (int i = 0; i < privateKey.length; ++i) {
+            retString.append(Integer.toHexString(0x0100 + (privateKey[i] & 0x00FF)).substring(1));
+        }
+        System.out.println("private key: " +retString);
+
+        Cipher cipher = Cipher.getInstance(pair.getPrivate().getAlgorithm());
+        cipher.init(Cipher.ENCRYPT_MODE, pair.getPrivate());
+        System.out.println("sig: "+cipher.doFinal("{}".getBytes(Charset.defaultCharset())).toString());
+        System.out.println(Hex.encodeHexString(cipher.doFinal(("Hello world").getBytes())));
     }
 }
